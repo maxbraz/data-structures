21d77f05541a444014b878dae0918a8f4df366f0
Max Braz <braz.max@gmail.com>
2017-03-29 12:19:39 -0700

    implemented queue in functional style.  fixed typo

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,29 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
+  var counter = 1;
+  var firstInLine = 1;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[counter] = value;
+    counter ++;
+    size ++;
   };
 
   someInstance.dequeue = function() {
+    var temp = storage[firstInLine];
+    delete storage[firstInLine];
+    firstInLine ++;
+    if (size !== 0) {
+      size --;
+    }
+    return temp;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
