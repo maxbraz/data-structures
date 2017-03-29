c56b63748054358ef8bb2eb288e72a1d0533bbf1
kvo180 <khoavo180@gmail.com>
2017-03-29 12:30:55 -0700

    Add working code for functional stack.js

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,42 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value;
+    count++;
+    return value;
   };
 
   someInstance.pop = function() {
+    var popVal = storage[count - 1];
+    delete storage[count - 1];
+    if (count > 0) {
+      count--;
+    }
+    return popVal;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
 };
+
+// var stack = Stack();
+// stack.push('a');
+// console.log(stack.size());
+// console.log(stack);
+
+// stack.push('b');
+// console.log(stack);
+// console.log(stack.size());
+
+
+// console.log(stack.pop());
+// console.log(stack);
+
+// console.log(stack.pop());
