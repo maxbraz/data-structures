3d2a0cff5e73d13530330651a35c0f4e1a824d52
Hack Reactor Student <student@hackreactor.com>
2017-03-31 11:42:33 -0700

    pass specs for set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,34 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  var result = false;
+
+  _.each(this._storage, function(str) {
+    if (str === item) {
+      result = true;
+    }
+  });
+
+  return result;
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  add O(1) constant
+  contains O(n) linear
+  remove O(1) constant
  */
