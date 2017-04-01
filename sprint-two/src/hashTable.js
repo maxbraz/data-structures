885adfbe23ef93995974cde3cfa81545612c7ee0
Hack Reactor Student <student@hackreactor.com>
2017-03-31 19:13:47 -0700

    implement insert & retrieve

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,55 +5,74 @@ var HashTable = function() {
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.searchBucket = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-
-  // make a variable bucket and set it to storage at index
-
-  // if the bucket is not undefined
-    // if the length of the bucket is one
-      // return the value of what is in index zero of bucket at index one
-    // or else if the length of the bucket is greater than one
-      // iterate over the length of the bucket
-        // if the value at index zero of each tuple is equal to k
-          //return the value at index one of the tuple
-
-    // return undefined;
-  // or else 
-    // return undefined
-
-};
-
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
     // create a variable called 'tuple', an empty limitedArray
-    // set index zero of tuple to be k(ey) and index one of tuple to be v(alue)
+  var tuple = LimitedArray(2);
+  // set index zero of tuple to be k(ey) and index one of tuple to be v(alue)
+  
+  if (this.retrieve(k) !== undefined) {
+    var bucket = this._storage.get(index);
+
+    for (var i = 0; i < bucket.length; i++) {
+      // if the value at index zero of each tuple is equal to k
+      if (bucket[i].get(0) === k) {
+        //return the value at index one of the tuple
+        bucket[i].set(1, v);
+      }
+    }
+  } else {
+    tuple.set(0, k);
+    tuple.set(1, v);
 
   // if element in storage at index is undefined
-    // create a variable called 'bucket', an empty array
-    // push tuple into bucket
-    // push bucket into the storage array
-  // or else
-    // push tuple into storage array at the index
+    if (this._storage.get(index) === undefined) {
+      // create a variable called 'bucket', an empty array
+      var bucket = [];
+      // push tuple into bucket
+      bucket.push(tuple);
+      // insert bucket into the storage array
+      this._storage.set(index, bucket);
+    // or else
+    } else {
+      // push tuple into bucket in storage array at the index
+      this._storage.set(index, tuple);
+    }
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   
-  // make a variable bucket and set it to a new limited array
 
   // if the bucket is not undefined
+  if (this._storage.get(index) !== undefined) {
+    // make a variable bucket and set it to an array
+    var bucket = this._storage.get(index);
     // if the length of the bucket is one
+    if (bucket.length === 1) {
       // return the value of what is in index zero of bucket at index one
+      return bucket[0].get(1);
     // or else if the length of the bucket is greater than one
+    } else {
       // iterate over the length of the bucket
+      for (var i = 0; i < bucket.length; i++) {
         // if the value at index zero of each tuple is equal to k
+        if (bucket[i].get(0) === k) {
           //return the value at index one of the tuple
+          return bucket[i].get(1);
+        }
+      }
 
-    // return undefined;
+      // return undefined if key not found;
+      return undefined;
+    }
+
   // or else 
+  } else {
     // return undefined
-  
+    return undefined;
+  }
 };
 
 HashTable.prototype.remove = function(k) {
